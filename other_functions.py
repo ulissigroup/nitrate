545bbd943554ef9131b2985ac86e974070cac301
richardtran415 <rit634989@gmail.com>
2021-05-04 17:59:09 -0400

    add name to data

--- other_functions.py
+++ other_functions.py
@@ -1,19 +1,23 @@
 from pymatgen.analysis.pourbaix_diagram import PourbaixDiagram, PourbaixPlotter
+from pymatgen import MPRester
 
 
-def pourbaix_stable(entry):
-    mpid = entry.data['material_id']
+def pourbaix_stable(all_entries, mprester=None):
+    mprester = mprester if mprester else MPRester()
+    entry = all_entries[0]
     comp = entry.composition
     chemsys = list(comp.as_dict().keys())
     entries = mprester.get_pourbaix_entries(chemsys)
     pbd = PourbaixDiagram(entries)
     mpids = []
-    for entry in pbd.unstable_entries:
+    for entry in pbd.stable_entries:
         if type(entry.entry_id).__name__ == 'str':
             mpids.append(entry.entry_id)
         else:
             mpids.extend(entry.entry_id)
-    if mpid in mpids:
-        return False
-    else:
-        return True
+    stable_entries = []
+    for entry in all_entries:
+        mpid = entry.data['material_id']
+        if mpid in mpids:
+            stable_entries.append(entry)
+:
