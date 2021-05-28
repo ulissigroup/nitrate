519fc584728c1c66c2e080049b31d0f7cc4b0e5b
richardtran415 <rit634989@gmail.com>
2021-05-28 00:20:29 -0400

    import

--- lmdb_generator.py
+++ lmdb_generator.py
@@ -2,7 +2,7 @@ from pymatgen import Composition, Structure
 from pymatgen.io.ase import AseAtomsAdaptor
 from ocpmodels.preprocessing import AtomsToGraphs
 from ocpmodels.datasets.trajectory_lmdb import TrajectoryLmdbDataset
-from single_point_lmdb import SinglePointLmdbDataset
+from ocpmodels.datasets.single_point_lmdb import SinglePointLmdbDataset
 import sys, os, glob
 sys.path.append(os.getcwd())
 from other_functions import str_to_hkl
