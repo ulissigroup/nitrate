6e7e9e85b9514a91f6b9f96136b27fbe5b3aecbc
richardtran415 <rit634989@gmail.com>
2022-03-22 22:50:10 +0000

    added notebook for figure 5

---
+++ NO3_heatmap_files/README.md
@@ -0,0 +1,6 @@
+# nitrate
+Files needed to run the analysis developed by Goldsmith Lab. Copied from the following repo:
+https://github.com/goldsmith-lab/ptru-alloy-no3rr-activity
+
+Please cite the following if you use any of these files:
+Wang, Z., Young, S. D., Goldsmith, B. R., & Singh, N. (2021). Supporting Information: Increasing electrocatalytic nitrate reduction activity by controlling adsorption through PtRu alloying. Journal of Catalysis, 395, 143–154. https://doi.org/10.1016/j.jcat.2020.12.031
---
+++ constants.py
@@ -0,0 +1,16 @@
+from pymatgen.core.periodic_table import Element
+
+all_tms = [str(el) for el in Element 
+           if el.is_transition_metal and str(el) \
+           not in ['Ac', 'La', 'Hg', 'Tc', 'Cd'] and el.number < 81]
+all_pairs = [pair for pair in itertools.permutations(all_tms, 2)]
+
+from pymatgen.analysis.cost import CostAnalyzer, CostDBElements, CostDBCSV
+costdb = CostDBCSV('../costdb_elements_2021.csv')
+costanalyzer = CostAnalyzer(costdb)
+
+Eads_ref_oc20 = {'*O': -7.646083600000001, '*N': -.17228045E+02/2}
+Eads_ref_dft = {'*O': -1.53343974, '*N': -3.11173655, '*H': -1.11587797}
+Eads_ref_rpbe = {'*O': -2.0552055, '*N': -3.6207807, '*H': -1.11587797}
+Ncorr_db = [-0.03508250142669311, 1.0122001719490594]
+Ocorr_db = [0.43857541907825404, 1.0530681561315451]
